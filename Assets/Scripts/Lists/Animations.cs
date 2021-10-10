edb9b0f2ed837dbf744cbc448076f602155dd507
GabrielBSL-UEA <gbsl.snf17@uea.edu.br>
2021-10-09 22:25:20 -0400

    Robot animation
    Robot animation

---
+++ Assets/Scripts/Lists/Animations.cs
@@ -0,0 +1,5 @@
+public enum Animations
+{
+    idle,
+    action
+}
--- Assets/Scripts/Robot/Parts/RightArm.cs
+++ Assets/Scripts/Robot/Parts/RightArm.cs
@@ -3,4 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "RightArm", menuName = "ScriptableObjects/Parts/RightArm", order = 5)]
 public class RightArm : RobotPart
 {
+    [Header("SpecialSprite")]
+    [SerializeField] private Sprite shoulderSprite;
+    public Sprite SpecialSprite() { return shoulderSprite; }
 }
