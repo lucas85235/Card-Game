957de396cfe92ac6f5050653e51f4d1967f500ee
Lucas Lima <lucas85235@gmail.com>
2022-02-23 11:58:52 -0400

    WIP 3
    Count time

--- Assets/Scripts/Multiplayer/RoundLoopMP.cs
+++ Assets/Scripts/Multiplayer/RoundLoopMP.cs
@@ -1,8 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
 
 public class RoundLoopMP : Round
 {
+    [Header("Setup")]
+    [SerializeField] private bool useTimer = true;
+    [SerializeField] private float timeToPlay = 10f;
+    [SerializeField] private Slider timeSlider;
+    [SerializeField] private Text timeText;
 
+    private TimerMP timer = new TimerMP();
+
+    private IEnumerator Start()
+    {
+        yield return new WaitUntil(() => PhotonNetwork.PlayerList.Length > 0);
+
+        timer.Timer = timeToPlay;
+        timeSlider.gameObject.SetActive(useTimer);
+        timeSlider.maxValue = timeToPlay;
+        
+        if (useTimer)
+        {
+            Round.i.EndTurn.AddListener(() => timer.SetTimerProperties());
+            timer.SetTimerProperties();
+        }
+    }
+
+    private void Update()
+    {
+        timer.CountTimer(() =>
+        {
+            timeSlider.gameObject.SetActive(false);
+            Round.i.StartTurn?.Invoke();
+        });
+
+        if (timer.StartTimer)
+        {
+            float totalTime = timeToPlay;
+            timeSlider.value = totalTime - (float) timer.TimerIncrementValue;
+            timeText.text = timer.TimerIncrementValue.ToString("#");
+        }
+    }
 }
