2face6a393972c819ee2a39960e26ac65452f4b5
GabrielBSL-UEA <gbsl.snf17@uea.edu.br>
2022-05-19 22:44:19 -0400

    correção no cálculo do crítico

--- Assets/Scripts/Components/Life.cs
+++ Assets/Scripts/Components/Life.cs
@@ -91,18 +91,18 @@ public class Life : MonoBehaviour
         {
             ignoreShield = usedCard.Piercing;
             hitChance = Mathf.Clamp(1f + (m_robot.CurrentRobotStats[Stats.evasion] - GameController.i.GetTheOtherRobot(m_robot).CurrentRobotStats[Stats.accuracy]) / 100, 0f, 1f) - usedCard.MissChance;
-            critChance = m_robot.CurrentRobotStats[Stats.critChance];
+            critChance = m_robot.CurrentRobotStats[Stats.critChance] / 100;
         }
 
         // Chance de errar o dano da carta
-        if (Random.Range(0f, 1f) > hitChance)
+        if (Random.Range(0f, 1f) >= hitChance)
         {
             GameController.i.ShowMessageText(transform.localScale.x > 0, Color.blue, "Miss!");
             return;
         }
 
         // Chance de aplicar um critico no ataque
-        if (Random.Range(0f, 1f) < critChance)
+        if (Random.Range(0f, 1f) <= critChance)
         {
             decrement += Mathf.FloorToInt(decrement / 2);
             addicionalMessage = "Crit! ";
