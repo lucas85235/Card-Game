a2ea6a55c5806cc4eeb7d5b0f026f17cb42ccb41
Lucas Lima <lucas85235@gmail.com>
2022-06-04 15:16:28 -0400

    Adjust inventory on new ui

--- Assets/Scripts/Managers/MenuManager.cs
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -19,39 +19,17 @@ public class MenuManager : MonoBehaviour
     [SerializeField] private GameObject cardInfoPrefab;
 
     [Header("UI")]
-    [SerializeField] private GameObject partContainer;
-    [SerializeField] private Transform selectedContainer;
-    [SerializeField] private Transform headOptionsContainer;
-    [SerializeField] private Transform leftArmOptionsContainer;
-    [SerializeField] private Transform rightArmOptionsContainer;
-    [SerializeField] private Transform torsoOptionsContainer;
-    [SerializeField] private Transform legsOptionsContainer;
+    [SerializeField] private GameObject partItem;
+    [SerializeField] private RectTransform selectedContainer;
 
     [Header("UI Hand Cards")]
     [SerializeField] private RectTransform handConfiner;
 
-    private List<Transform> robotPartOrder = new List<Transform>();
+    private List<RobotPartItem> newParts = new List<RobotPartItem>();
 
     private void Awake()
     {
-        SetRobotPartOrder();
-        LoadTestData();
-
-        robotAnimation.ChangeRobotSprites(DataManager.Instance.GetCurrentRobot());
-    }
-
-    private void SetRobotPartOrder()
-    {
-        robotPartOrder.Add(headOptionsContainer);
-        robotPartOrder.Add(leftArmOptionsContainer);
-        robotPartOrder.Add(legsOptionsContainer);
-        robotPartOrder.Add(rightArmOptionsContainer);
-        robotPartOrder.Add(torsoOptionsContainer);
-    }
-
-    private void LoadTestData()
-    {
-        var newParts = new List<RobotPartItem>();
+        newParts = new List<RobotPartItem>();
         int countParts = 0;
 
         foreach (var item in Enum.GetValues(typeof(PartID)))
@@ -67,8 +45,42 @@ public class MenuManager : MonoBehaviour
         for (int i = 0; i < newParts.Count; i++)
         {
             DataManager.Instance.AddPartItem(newParts[i], "code" + (i + 1));
+        }
 
-            GameObject newOption = Instantiate(partContainer, robotPartOrder[i % 5]);
+        robotAnimation.ChangeRobotSprites(DataManager.Instance.GetCurrentRobot());
+    }
+
+    private void LoadTestData()
+    {
+        newParts = new List<RobotPartItem>();
+        int countParts = 0;
+
+        foreach (var item in Enum.GetValues(typeof(PartID)))
+        {
+            newParts.Add(new RobotPartItem()
+            {
+                itemID = item.ToString()
+            });
+
+            countParts++;
+        }
+
+        for (int i = 0; i < newParts.Count; i++)
+        {
+            DataManager.Instance.AddPartItem(newParts[i], "code" + (i + 1));
+        }
+    }
+
+    private void FillPartsInformation(RobotParts part)
+    {
+        foreach (RectTransform oldCard in selectedContainer)
+            Destroy(oldCard.gameObject);
+
+        for (int i = 0; i < newParts.Count; i++)
+        {
+            if (i % 5 != (int)part) continue;
+
+            GameObject newOption = Instantiate(partItem, selectedContainer);
             newOption.TryGetComponent(out PartOptionButton partOption);
             partOption.PartCode = "code" + (i + 1);
             partOption.orderLayer = i % 5;
@@ -76,51 +88,30 @@ public class MenuManager : MonoBehaviour
             newOption.TryGetComponent(out Button optionButton);
             optionButton.onClick.AddListener(() =>
             {
-                if (optionButton.transform.parent == selectedContainer)
-                {
-                    return;
-                }
+                Debug.Log("Select Part");
 
                 newOption.TryGetComponent(out PartOptionButton partOption);
                 DataManager.Instance.AssignPartToRobot(partOption.PartCode);
                 DataManager.Instance.SavePart(partOption.PartCode, partOption.orderLayer);
 
-                int oldIndex = optionButton.transform.GetSiblingIndex();
-                Transform oldSelected = selectedContainer.GetChild(partOption.orderLayer);
-
-                oldSelected.transform.SetParent(robotPartOrder[partOption.orderLayer]);
-                oldSelected.transform.SetSiblingIndex(oldIndex);
-
-                optionButton.transform.SetParent(selectedContainer);
-                optionButton.transform.SetSiblingIndex(partOption.orderLayer);
+                // Select Feedback
+                newOption.transform.GetChild(0).TryGetComponent(out Image buttonSelect);
+                buttonSelect.color = Color.green;
 
                 PlayClickSound();
                 FillRobotInformation();
-                FillRobotPartInformation((int)RobotParts.Head);
+                FillRobotPartInformation((int)part);
             });
+
+            newOption.transform.GetChild(0).TryGetComponent(out Image buttonSelect);
+            if (DataManager.Instance.data.SaveCodes.Contains("code" + (i + 1)))
+            {
+                buttonSelect.color = Color.green;
+            }
+            else buttonSelect.color = Color.white;
 
             newOption.transform.GetChild(0).GetChild(0).TryGetComponent(out Image buttonImage);
             buttonImage.sprite = DataManager.Instance.GetPartSprite("code" + (i + 1));
-        }
-
-        if (DataManager.Instance.data.SaveCodes.Count > 0)
-        {
-            for (int i = 0; i < DataManager.Instance.data.SaveCodes.Count; i++)
-            {
-
-                robotPartOrder[i].GetChild(0).transform.SetParent(selectedContainer);
-                DataManager.Instance.AssignPartToRobot(DataManager.Instance.data.SaveCodes[i]);
-                DataManager.Instance.SavePart(DataManager.Instance.data.SaveCodes[i], i);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                robotPartOrder[i].GetChild(0).transform.SetParent(selectedContainer);
-                DataManager.Instance.AssignPartToRobot("code" + (i + 1));
-                DataManager.Instance.SavePart("code" + (i + 1), i);
-            }
         }
     }
 
@@ -192,29 +183,31 @@ public class MenuManager : MonoBehaviour
 
     public void FillRobotPartInformation(int part)
     {
+        FillPartsInformation((RobotParts)part);
+
         var robot = DataManager.Instance.GetCurrentRobot();
 
-        switch ((RobotParts) part)
+        switch ((RobotParts)part)
         {
-            case RobotParts.Head: 
-                FillRobotPartInformation(robot.GetHead().Cards()); 
-            break;
+            case RobotParts.Head:
+                FillRobotPartInformation(robot.GetHead().Cards());
+                break;
 
-            case RobotParts.Torso: 
-                FillRobotPartInformation(robot.GetTorso().Cards()); 
-            break;
+            case RobotParts.Torso:
+                FillRobotPartInformation(robot.GetTorso().Cards());
+                break;
 
-            case RobotParts.LeftArm: 
-                FillRobotPartInformation(robot.GetLeftArm().Cards()); 
-            break;
+            case RobotParts.LeftArm:
+                FillRobotPartInformation(robot.GetLeftArm().Cards());
+                break;
 
-            case RobotParts.RightArm: 
-                FillRobotPartInformation(robot.GetRightArm().Cards()); 
-            break;
+            case RobotParts.RightArm:
+                FillRobotPartInformation(robot.GetRightArm().Cards());
+                break;
 
-            case RobotParts.Legs: 
-                FillRobotPartInformation(robot.GetLeg().Cards()); 
-            break;
+            case RobotParts.Legs:
+                FillRobotPartInformation(robot.GetLeg().Cards());
+                break;
         }
     }
 
